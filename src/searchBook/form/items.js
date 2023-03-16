d72afa28dcba4551241beb3aab7bc82367dc3f18
Suxrob Oktamov <oktamovsuxrob0@gmail.com>
2023-03-16 07:18:46 +0500

    .

--- src/searchBook/form/items.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function items(item){
-    return(
-        <li className="list-group-item">{item.name}<button className="btn btn-danger btn-sm float-end delete">X</button></li>
-    )
-};
-
-export default items;
